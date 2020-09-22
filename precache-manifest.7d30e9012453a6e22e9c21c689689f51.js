f65c81c8cc60dae6d99b53050a3a90974d77ae12
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-22 05:51:34 +0000

    Deploy rayspock/article-example to github.com/rayspock/article-example.git:gh-pages

---
+++ precache-manifest.7d30e9012453a6e22e9c21c689689f51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6002f42321adc6cc0bd1ea2ab3afcca8",
+    "url": "/article-example/index.html"
+  },
+  {
+    "revision": "d4b3857d2451dd7dda99",
+    "url": "/article-example/static/css/2.2058ee32.chunk.css"
+  },
+  {
+    "revision": "d4b3857d2451dd7dda99",
+    "url": "/article-example/static/js/2.2227b13e.chunk.js"
+  },
+  {
+    "revision": "f88b36470b8b650da4a18244e35dd001",
+    "url": "/article-example/static/js/2.2227b13e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aacfe17916b5b73f475f",
+    "url": "/article-example/static/js/main.df492666.chunk.js"
+  },
+  {
+    "revision": "c5b37b93d2372f786c50",
+    "url": "/article-example/static/js/runtime-main.b1cba027.js"
+  }
+]);
